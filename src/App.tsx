ccfce26cb735ff204a966f59308f03ac409b8bc0
Can Bulgay <can.bulgay@enuygun.com>
2025-04-01 17:54:30 +0300

    Add gh-pages configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import EventBubblingPage from "./pages/EventBubbling/App";
 import "./App.css";
 
